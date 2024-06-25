f79a88d68158238f02878755f49cfccf7ece3b65
kenyu1023mori <kenyu1023mori@gmail.com>
2024-06-25 09:43:05 +0900

    テンプレートの追加

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+
+int main() {
+
+    return 0;
+}
