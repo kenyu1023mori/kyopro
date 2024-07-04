d2328fba9de0622b3b79f8c2d068885a5905243e
kenyu1023mori <kenyu1023mori@gmail.com>
2024-07-05 01:11:17 +0900

    累積和，まだ余裕

---
+++ tessoku/sec02/a06.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N);
+    rep(i, N) cin >> A[i];
+    vector<int> acc(N+1);
+    for (int i = 1; i <= N; i++) acc[i] = acc[i-1]+A[i-1];
+    rep(i, Q) {
+        int l, r;
+        cin >> l >> r;
+        cout << acc[r]-acc[l-1] << endl;;
+    }
+    return 0;
+}
