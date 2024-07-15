5a42b2c6324dcc6d5fb769fb1396187943651262
kenyu1023mori <kenyu1023mori@gmail.com>
2024-07-16 08:54:59 +0900

    途中

--- tessoku/sec02/a07.cpp
+++ tessoku/sec02/a07.cpp
@@ -8,15 +8,12 @@ using ull = unsigned long long;
 int main() {
     int D, N;
     cin >> D >> N;
-    vector<int> L(N), R(N);
-    rep(i, N) cin >> L[i] >> R[i];
-    sort(L.begin(), L.end());
-    sort(R.begin(), R.end());
-    int l = 0, r = 0;
-    for (int i = 1; i <= D; i++) {
-        while (L[l] == i) l++;
-        while (R[r]+1 == i) r++;
-        cout << l-r << endl;
+    vector<int> A(D+1);
+    rep(i, N) {
+        int l, r;
+        cin >> l >> r;
+        A[l]++;
+        A[r]--;
     }
     return 0;
 }
