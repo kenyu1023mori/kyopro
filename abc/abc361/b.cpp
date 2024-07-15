8879e5dda7b88dfcb6fa3598c9969fd8c854306f
kenyu1023mori <kenyu1023mori@gmail.com>
2024-07-16 08:55:41 +0900

    途中

---
+++ abc/abc361/b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+
+int main() {
+    int a, b, c, d, e, f, g, h, i, j, k, l;
+    cin >> a >> b >> c >> d >> e >> f;
+    cin >> g >> f >> i >> j >> k >> l;
+    return 0;
+}
