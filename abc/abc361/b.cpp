ce238be1f46cbe6e71289ddf5b678e7f3f68b038
kenyu1023mori <kenyu1023mori@gmail.com>
2024-07-16 10:01:51 +0900

    3次元の問題を1次元に落とす，共通部分を持つかどうかの判定，色々使えそう

--- abc/abc361/b.cpp
+++ abc/abc361/b.cpp
@@ -5,9 +5,16 @@ using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
 
+bool f(int l1, int r1, int l2, int r2) {
+    return !(r1 <= l2 or r2 <= l1);
+}
 int main() {
-    int a, b, c, d, e, f, g, h, i, j, k, l;
-    cin >> a >> b >> c >> d >> e >> f;
-    cin >> g >> f >> i >> j >> k >> l;
+    int x1, y1, z1, x2, y2, z2;
+    int x3, y3, z3, x4, y4, z4;
+    cin >> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
+    cin >> x3 >> y3 >> z3 >> x4 >> y4 >> z4;
+    if (f(x1, x2, x3, x4) and f(y1, y2, y3, y4) and f(z1, z2, z3, z4)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
     return 0;
 }
