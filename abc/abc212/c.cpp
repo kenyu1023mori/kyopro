13628156abb5fed53dc995a83f25e528168614c5
kenyu1023mori <kenyu1023mori@gmail.com>
2024-07-17 13:55:42 +0900

    chmin使ってみた．あと解説記事通り静的配列でやったけどvectorのがやっぱ良いらしい

--- abc/abc212/c.cpp
+++ abc/abc212/c.cpp
@@ -4,29 +4,38 @@ using namespace std;
 using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
+template <typename T>
+bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
 
 // bをソートして，aiに対して一番近い値を二分探索で探してmin
 
 int main() {
     ll n, m;
     cin >> n >> m;
-    vector<ll> a(n), b(m);
+    ll a[n], b[m];
     rep(i, n) cin >> a[i];
     rep(i, m) cin >> b[i];
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+    sort(a, a+n);
+    sort(b, b+m);
     ll ans = LLONG_MAX;
     rep(i, n) {
-        auto iter = lower_bound(b.begin(), b.end(), a[i]);
-        if (iter != b.begin()) {
-            int idx1 = distance(b.begin(), iter);
-            int idx2 = distance(b.begin(), iter-1);
-            ans = min({ans, abs(a[i]-b[idx1]), abs(a[i]-b[idx2])});
-        }
-        else {
-            int idx1 = distance(b.begin(), iter);
-            ans = min(ans, abs(a[i]-b[idx1]));
-        }
+        int j = lower_bound(b, b+m, a[i]) - b;
+        if (j < m) chmin(ans, abs(a[i]-b[j]));
+        if (0 <= j-1) chmin(ans, abs(a[i]-b[j-1]));
     }
     cout << ans << endl;
     return 0;
