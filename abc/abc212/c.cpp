c29529d35a835679ddd63762cab1b13509302202
kenyu1023mori <kenyu1023mori@gmail.com>
2024-07-17 13:40:51 +0900

    配列Bの長さNじゃなくてMだった...

--- abc/abc212/c.cpp
+++ abc/abc212/c.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, n) for (ll i = 0; i < (n); i++)
 using namespace std;
 using uint = unsigned int;
 using ll = long long;
@@ -10,22 +10,22 @@ using ull = unsigned long long;
 int main() {
     ll n, m;
     cin >> n >> m;
-    vector<ll> a(n), b(n);
+    vector<ll> a(n), b(m);
     rep(i, n) cin >> a[i];
-    rep(i, n) cin >> b[i];
+    rep(i, m) cin >> b[i];
+    sort(a.begin(), a.end());
     sort(b.begin(), b.end());
     ll ans = LLONG_MAX;
     rep(i, n) {
         auto iter = lower_bound(b.begin(), b.end(), a[i]);
-
-        if (iter != b.end()) {
-            int idx1 = distance(b.begin(), iter);
-            ans = min(ans, abs(a[i] - b[idx1]));
-        }
         if (iter != b.begin()) {
-            iter--;
-            int idx2 = distance(b.begin(), iter);
-            ans = min(ans, abs(a[i] - b[idx2]));
+            int idx1 = distance(b.begin(), iter);
+            int idx2 = distance(b.begin(), iter-1);
+            ans = min({ans, abs(a[i]-b[idx1]), abs(a[i]-b[idx2])});
+        }
+        else {
+            int idx1 = distance(b.begin(), iter);
+            ans = min(ans, abs(a[i]-b[idx1]));
         }
     }
     cout << ans << endl;
