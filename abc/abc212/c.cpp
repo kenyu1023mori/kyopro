0bc440bf68679a86c2dd4f7953130fd8ac7bf64c
kenyu1023mori <kenyu1023mori@gmail.com>
2024-07-17 13:56:46 +0900

    vectorで書き直した．イテレータの扱いがやっぱ注意

--- abc/abc212/c.cpp
+++ abc/abc212/c.cpp
@@ -4,6 +4,7 @@ using namespace std;
 using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
+
 template <typename T>
 bool chmin(T& a, T b) {
     if (a > b) {
@@ -12,6 +13,7 @@ bool chmin(T& a, T b) {
     }
     return false;
 }
+
 template <typename T>
 bool chmax(T& a, T b) {
     if (a < b) {
@@ -21,21 +23,19 @@ bool chmax(T& a, T b) {
     return false;
 }
 
-// bをソートして，aiに対して一番近い値を二分探索で探してmin
-
 int main() {
     ll n, m;
     cin >> n >> m;
-    ll a[n], b[m];
+    vector<ll> a(n), b(m);
     rep(i, n) cin >> a[i];
     rep(i, m) cin >> b[i];
-    sort(a, a+n);
-    sort(b, b+m);
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
     ll ans = LLONG_MAX;
     rep(i, n) {
-        int j = lower_bound(b, b+m, a[i]) - b;
-        if (j < m) chmin(ans, abs(a[i]-b[j]));
-        if (0 <= j-1) chmin(ans, abs(a[i]-b[j-1]));
+        int j = lower_bound(b.begin(), b.end(), a[i]) - b.begin();
+        if (j < m) chmin(ans, abs(a[i] - b[j]));
+        if (0 <= j - 1) chmin(ans, abs(a[i] - b[j - 1]));
     }
     cout << ans << endl;
     return 0;
