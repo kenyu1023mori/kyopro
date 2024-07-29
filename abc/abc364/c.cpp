56287c47ff1f45fb985b62d188cd9062ce45d8e1
kenyu1023mori <kenyu1023mori@gmail.com>
2024-07-29 10:05:34 +0900

    解説読んだ。にぶたんも累積和もいらんやんけ。いつもどおり難しく考えすぎ

--- abc/abc364/c.cpp
+++ abc/abc364/c.cpp
@@ -34,19 +34,16 @@ int main() {
     sort(B.begin(), B.end());
     reverse(A.begin(), A.end());
     reverse(B.begin(), B.end());
-    vector<ull> acc_A(N+1);
-    vector<ull> acc_B(N+1);
-    rep(i, 1, N+1) {
-        ull a = A[i-1];
-        acc_A[i] = acc_A[i-1]+a;
+    ull now_A = 0;
+    ull now_B = 0;
+    rep(i, 0, N) {
+        now_A += A[i];
+        now_B += B[i];
+        if (now_A > X or now_B > Y) {
+            cout << i+1 << endl;
+            return 0;
+        }
     }
-    rep(i, 1, N + 1) {
-        ull b = B[i-1];
-        acc_B[i] = acc_B[i - 1] + b;
-    }
-    int i = upper_bound(acc_A.begin(), acc_A.end(), X) - acc_A.begin();
-    int j = upper_bound(acc_B.begin(), acc_B.end(), Y) - acc_B.begin();
-    if (min(i, j) <= N) cout << min(i, j) << endl;
-    else cout << N << endl;
+    cout << N << endl;
     return 0;
 }
